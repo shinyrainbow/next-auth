8f29d4a30bbd95df3dcfb07f1c9e8653774b2096
shinyrainbow <kornkanok.lieng@gmail.com>
2025-08-01 22:48:39 +0700

    integrate with prisma

---
+++ auth.ts
@@ -0,0 +1,20 @@
+// import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
+// import Google from "next-auth/providers/google"
+ 
+// export const { auth, handlers, signIn, signOut } = NextAuth({
+//   providers: [GitHub, Google],
+// })
+
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import authConfig from "./auth.config"
+
+const prisma = new PrismaClient()
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+})
